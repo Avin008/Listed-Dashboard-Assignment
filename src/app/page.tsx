6269183a7e1d72a16c04b87e331100876de6b684
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-06-22 15:24:56 +0530

    added: brand component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Brand } from "@/components";
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-2xl">Hello World</h1>
-      <p>This is where the magic happens!</p>
+    <main className="grid h-screen grid-cols-8 bg-background">
+      <div className="col-span-3 flex items-center justify-center border border-black bg-[#000000]">
+        <Brand />
+      </div>
+      <div className="col-span-5 border border-black"></div>
     </main>
   );
 }
---
+++ src/components/Brand.tsx
@@ -0,0 +1,9 @@
+const Brand = () => {
+  return (
+    <div>
+      <h1 className="text-[72px] text-white">Board.</h1>
+    </div>
+  );
+};
+
+export default Brand;
