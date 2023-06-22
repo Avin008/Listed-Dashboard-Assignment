72b7c1c00ad8c6898e4e944fa694e67fae20ee15
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-06-22 20:34:51 +0530

    added: navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,12 +7,22 @@ const Navbar = () => {
       <span>
         <h1 className="font-sans text-[24px] font-bold">Dashboard</h1>
       </span>
-      <ul className="flex items-center gap-5 pr-6">
+      <ul className="flex items-center gap-8 pr-6">
         <li>
           <Searchbar />
         </li>
-        <li>notification</li>
-        <li>user</li>
+        <li>
+          <Image src="/bell.png" alt="" width={18} height={20} />
+        </li>
+        <li>
+          <Image
+            className="rounded-full"
+            src="/profile.png"
+            alt=""
+            width={35}
+            height={35}
+          />
+        </li>
       </ul>
     </nav>
   );
