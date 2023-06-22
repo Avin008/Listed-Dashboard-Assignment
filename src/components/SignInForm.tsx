dd54227f06740af81ec86c540546e01bb8ca5ed3
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-06-22 15:53:54 +0530

    added: login-form component

---
+++ src/components/SignInForm.tsx
@@ -0,0 +1,32 @@
+const SignInForm = () => {
+  return (
+    <form className="h-[317px] w-[385px] space-y-4 rounded-[20px] bg-[#FFFFFF] p-[30px]">
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email">Email address</label>
+        <input
+          className="h-[40px] rounded-[10px] bg-[#F5F5F5] px-4"
+          type="email"
+          placeholder="johndoe@gmail.com"
+        />
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email">Password</label>
+        <input
+          className="h-[40px] rounded-[10px] bg-[#F5F5F5] px-4"
+          type="password"
+          placeholder="••••••••"
+        />
+      </div>
+      <div className="">
+        <span className="text-[#346BD4]">Forget Password?</span>
+      </div>
+      <div className="flex">
+        <button className="h-[40px] w-full rounded-[10px] bg-[#000000] font-bold text-[#FFFFFF]">
+          Sign In
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SignInForm;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 import Brand from "./Brand";
+import SignInForm from "./SignInForm";
 
-export { Brand };
+export { Brand, SignInForm };
