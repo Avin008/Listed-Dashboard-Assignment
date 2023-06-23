c087a072e862c4dc8eccb772e2bb3f875cd8a322
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-06-24 01:19:51 +0530

    refactor: pie-chart

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -8,11 +8,13 @@ ChartJS.register(CategoryScale);
 
 const PieChart = ({ data }: { data: any }) => {
   const [chartData, setChartData] = useState({
-    labels: data.map((data: any) => data.year),
+    labels: ["Basic Tees", "Custom Short Pants", "Super Hoodies"],
     datasets: [
       {
         label: "Guest",
-        data: data.map((x: any) => x.userGained),
+        data: [10, 30, 60],
+        backgroundColor: ["#EE8484", "#F6DC7D", "#98D89E"],
+        borderWidth: 0,
       },
     ],
   });
@@ -24,7 +26,17 @@ const PieChart = ({ data }: { data: any }) => {
         radius: 80,
         aspectRatio: 2,
         plugins: {
-          legend: { align: "center", position: "right" },
+          legend: {
+            align: "center",
+            position: "right",
+            labels: {
+              pointStyle: "circle",
+              usePointStyle: true,
+              padding: 30,
+              font: { weight: "800" },
+              color: "#000000",
+            },
+          },
         },
         responsive: true,
       }}
