98d42104606dc940e958044bcb0322f3c43696ac
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-06-24 02:39:11 +0530

    added: apos

--- src/components/CreateAccountLink.tsx
+++ src/components/CreateAccountLink.tsx
@@ -2,7 +2,7 @@ const CreateAccountLink = () => {
   return (
     <div className="flex justify-center text-[16px]">
       <span className="font-mono text-[#858585]">
-        Don't have an account?{" "}
+        Don&apos;t have an account?{" "}
         <span className="text-[#346BD4]">Register here</span>
       </span>
     </div>
