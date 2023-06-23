79ca130fa7eb7ccd97b0fa00d9d09d6b42dae3ef
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-06-24 02:01:09 +0530

    style: changed body margin

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ import { lineGraphData, cardData } from "../../../data";
 
 const DashboardPage = () => {
   return (
-    <div className="mx-auto mt-[35px] w-[95%] space-y-5">
+    <div className="mx-auto mt-[25px] w-[95%] space-y-5">
       <Navbar />
       <div className="mt-[40px] grid gap-6 x_sm:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {cardData.map((x) => (
