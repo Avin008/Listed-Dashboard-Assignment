33a9357c5ece16b18a5640c118990e36b528d28d
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-06-24 23:43:11 +0530

    added: api call

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -79,15 +79,17 @@ const DashboardPage = () => {
   return (
     <div className="mx-auto mt-[25px] w-[95%] space-y-5">
       <Navbar />
-      <div className="mt-[40px] grid gap-6 x_sm:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {StatsCardData?.map((cardData) => (
-          <StatsCard key={cardData.id} cardData={cardData} />
-        ))}
-      </div>
+      {StatsCardData.length && (
+        <div className="mt-[40px] grid gap-6 x_sm:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {StatsCardData?.map((cardData) => (
+            <StatsCard key={cardData.id} cardData={cardData} />
+          ))}
+        </div>
+      )}
       {lineChartData.length && <LineChartCard data={lineChartData} />}
       <div className="grid gap-8 sm:grid-cols-1 xl:grid-cols-2">
         {pieChartData.length && <PieChartCard data={pieChartData} />}
-        <ScheduleCard cardData={scheduleData} />
+        {scheduleData.length && <ScheduleCard cardData={scheduleData} />}
       </div>
     </div>
   );
